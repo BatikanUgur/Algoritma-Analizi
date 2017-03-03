c835d7b72e5e730fae6a92ee089136ae08ab2bb3
BatikanUgur <batikanugurtizer@gmail.com>
2017-03-03 13:03:29 +0200

    0(logb) c++ adım sayısı

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 #include <math.h>
-//0(logb) c++
+// 0(logb) c++ adým sayýsý
 using namespace std;
-
+int static i=0;
 long pow(int x,int n){
-if(n==0) return 1;
-if(n==1) return x;
+if(n==0){
+    i++;
+    return 1;
+}
+if(n==1){
+    i++;
+    return x;
+}
 if(n%2==0){
+    i++;
     return pow(x*x,n/2);
 }
 
 else{
+    i++;
     return pow(x*x,n/2)*x;
 }
+
 }
+
+
 int main()
 {
-    long summary=pow(6,20);
+    long sum=pow(6,20);
 
-    cout << "summary= " << summary<< endl;
+    cout << "summary= " << sum << endl;
+    cout<< "number of steps = "<<i<<endl;
     return 0;
 }
+
